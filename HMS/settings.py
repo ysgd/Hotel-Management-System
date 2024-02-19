ef76e4d3d25571d4fc6a0156e10e648be1b34c75
Yaju Shrestha <yajustha11@gmail.com>
2024-02-19 14:38:52 +0545

    added user_auth_model

--- HMS/settings.py
+++ HMS/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'management',
+    'accounting',
+    'frontdesk',
+    'restaurant',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -86,7 +90,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
-AUTH_USER_MODEL = []
+AUTH_USER_MODEL = 'management.User'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
