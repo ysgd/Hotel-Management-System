a1eaa94122949918f15305238870449c755eab1c
Yaju Shrestha <yajustha11@gmail.com>
2024-02-19 14:41:04 +0545

    created class for RoomTypeView

--- management/views.py
+++ management/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import RoomTypeSerializer
+from .models import RoomType
 
 # Create your views here.
+class RoomTypeView(ModelViewSet):
+  queryset = RoomType.objects.all()
+  serializer_class = RoomTypeSerializer
