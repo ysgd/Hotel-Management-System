90a62528eb53d111ad635515a6b5f0d4672ba11a
Yaju Shrestha <yajustha11@gmail.com>
2024-02-24 20:46:30 +0545

    added RoomSerializer

--- management/serializers.py
+++ management/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import RoomType
+from .models import RoomType,Room
 
 class RoomTypeSerializer(serializers.ModelSerializer):
   class Meta:
     model = RoomType
+    fields = '__all__'
+    
+class RoomSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Room
     fields = '__all__'
