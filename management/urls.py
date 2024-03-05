746751436167ac26b2f9cd9f45ed385cb4486068
Yaju Shrestha <yajustha11@gmail.com>
2024-03-05 19:20:58 +0545

    added path for role

--- management/urls.py
+++ management/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomTypeView, RoomView, RoomEditView, UserView
+from .views import RoomTypeView, RoomView, RoomEditView, UserView, GroupView
 
 urlpatterns = [
     path('room-type/',RoomTypeView.as_view({'get':'list','post':'create'})),
@@ -7,5 +7,6 @@ urlpatterns = [
     path('room/',RoomView.as_view()),
     path('room/<int:pk>/',RoomEditView.as_view()),
     path('register/',UserView.as_view({'post':'register'}),name='register'),
-    path('login/',UserView.as_view({'post':'login'}),name='login')
+    path('login/',UserView.as_view({'post':'login'}),name='login'),
+    path('role/',GroupView.as_view({'get':'list'}),name='role-listing')
 ]
